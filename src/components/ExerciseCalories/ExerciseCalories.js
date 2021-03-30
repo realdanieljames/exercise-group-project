ffcc7c1d4f340169a2d3af66d63e7f8904502a9c
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2021-03-30 12:33:26 -0600

    created food and exercise components template

---
+++ src/components/ExerciseCalories/ExerciseCalories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExerciseCalories = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ExerciseCalories
---
+++ src/components/FoodCalories/FoodCalories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FoodCalories = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FoodCalories
