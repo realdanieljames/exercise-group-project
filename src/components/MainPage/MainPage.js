abd8bdb6f12f73db43c2e0ee5832bb84fd439e32
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2021-03-31 11:59:48 -0600

    exercise component working

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,20 +1,50 @@
-import React from "react";
+import React, {createRef, useRef} from "react";
 import { connect } from "react-redux";
-
+import './MainPage.css'
 
 
 const MainPage = (props) => {
+
+
+const exerciseRef = createRef('')
+const exerciseCalorieRef = createRef('')
 return (
     <div>
     <div>
         <p>Main Page</p>
     </div>
 
+
+{/* Exercise Component */}
+
+    <div style={{border: "1px solid black"}}>
+    <h2>Add Exercise</h2>
     <div>
+        <input placeholder="Type Exercise Here" ref={exerciseRef} />
+        <input placeholder="Type Calories Here" ref={exerciseCalorieRef} />
+        <button onClick={() => props.addExercise(exerciseCalorieRef, exerciseRef)} >Add Exercise</button>
+        
+        {props.exercise.map((exercise,i) => {
+            return (
+                <div>
+                    <u>Exercise {i+1}</u><br></br>
+                    {exercise.name}<br></br>
+                    Calories: {exercise.calories}
+                </div>
+            )
+        })}
+    </div>
+
+    </div>
+
+{/* Food Component */}
+<div style={{border: "1px solid black"}}>
+    <h2>Meals</h2>
         {/* {props.food.map((value)=>{
             console.log(value)
         })} */}
     </div>
+    
     </div>
 );
 };
@@ -22,6 +52,7 @@ return (
 const mapStateToProps = (state) => {
     return {
         food: state.food_Reducer.food,
+        exercise: state.exercise_Reducer.exercise
         
     }
 }
@@ -30,7 +61,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         addFood: () => dispatch({type:"ADD_NEW_FOOD", newFood: {name: "Cheddaer CHd", id: "ps1234", calories: "245"}}),
-        // addClassToStudent: (id, newClass) => dispatch({type: "ADD_CLASS_TO_STUDENT", targetId: id, class: newClass})
+        addExercise: (exerciseCalorieRef, exerciseRef) => dispatch({type: "ADD_NEW_EXERCISE", exercise:{id: 1, name: exerciseRef.current.value, calories: exerciseCalorieRef.current.value} })
     }
 }
 
