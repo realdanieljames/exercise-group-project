b6190bbda9d4e3d06be574ab87bab87898c23660
DANIEL JAMES <daniel.james@codeimmersives.com>
2021-03-31 16:40:40 -0400

    accepted and merged wwith current changes from main branch

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,70 +1,55 @@
 import React, {createRef, useRef} from "react";
-import './MainPage.css'
 import { connect } from "react-redux";
+import './MainPage.css'
+import ExerciseCalories from '../../components/ExerciseCalories/ExerciseCalories';
 import FoodCalories from "../FoodCalories/FoodCalories";
 
 
-
 const MainPage = (props) => {
-
-
-const exerciseRef = createRef('')
-const exerciseCalorieRef = createRef('')
+    console.log(props);
 return (
     <div>
     <div>
         <p>Main Page</p>
     </div>
-    {/* {props.food.map((value)=>{
-        return(
-            <FoodCalories/> */}
 
 
 {/* Exercise Component */}
+        <ExerciseCalories
+         exerciseState={props.exercise} 
+         addExercise={props.addExercise}
+         calories={props.calories}
+          />
 
-    <div style={{border: "1px solid black"}}>
-    <h2>Add Exercise</h2>
-    <div>
-        <input placeholder="Type Exercise Here" ref={exerciseRef} />
-        <input placeholder="Type Calories Here" ref={exerciseCalorieRef} />
-        <button onClick={() => props.addExercise(exerciseCalorieRef, exerciseRef)} >Add Exercise</button>
-        
-        {props.exercise.map((exercise,i) => {
-            return (
-                <div>
-                    <u>Exercise {i+1}</u><br></br>
-                    {exercise.name}<br></br>
-                    Calories: {exercise.calories}
-                </div>
-            )
-        })}
-    </div>
 
-    </div>
 
-{/* Food Component */}
+          {/* Food Component */}
 <div style={{border: "1px solid black"}}>
 
-    <FoodCalories props={props}/>
+<FoodCalories props={props}/>
 
-    </div>
-    
-    </div>
+</div>
+
+</div>
 );
 };
+//     </div>
+// )
+// }
 
 const mapStateToProps = (state) => {
     return {
         food: state.food_Reducer.food,
-        exercise: state.exercise_Reducer.exercise
+        exercise: state.exercise_Reducer.exercise,
+        calories: state.exercise_Reducer.calories
         
-
-}}
-
+    }
+}
 
 
 const mapDispatchToProps = (dispatch) => {
     return {
+        // addFood: () => dispatch({type:"ADD_NEW_FOOD", newFood: {name: "Cheddaer CHd", id: "ps1234", calories: "245"}}),
         addFood: (newMealName, newCalories) => dispatch({type:"ADD_NEW_FOOD", newFood: {mealName: newMealName, id: "", calories: newCalories}}),
         addExercise: (exerciseCalorieRef, exerciseRef) => dispatch({type: "ADD_NEW_EXERCISE", exercise:{id: 1, name: exerciseRef.current.value, calories: exerciseCalorieRef.current.value} })
     }
