81d001931dd1103844400cc8aba47cf43b993662
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2021-04-02 16:50:45 -0600

    Register working

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,31 @@
-import React, {createRef} from 'react'
+import React, {createRef, useState} from 'react'
 import {withRouter} from 'react-router-dom'
 import { connect } from "react-redux";
+import validator from 'validator';
+
 import "./Login.css"
 const Login = (props) => {
 
     const userNameRef = createRef()
     const passwordRef = createRef()
 
+    const [emailError, setEmailError] = useState(false)
+
+
     const switchToRegisterPage = () => {
         props.history.push("/register")
     }
+
+    const handleSubmit = () => {
+        if(!validator.isEmail(userNameRef.current.value)){
+            setEmailError(true)
+            return
+        }else {
+            setEmailError(false)
+        }
+        
+    }
+
 console.log(props);
     return (
         <div>
@@ -17,7 +33,8 @@ console.log(props);
             <p>Login Page</p>
             <input type='email'ref={userNameRef} placeholder="Email"/> <br></br>
             <input type='Password' ref={passwordRef} placeholder="Password" /><br></br>
-            <button>Submit</button>
+            {emailError ? <div>Please input a correct email.</div>: null}
+            <button onClick={handleSubmit}>Submit</button>
             <p>Not Registered? Sign up <span onClick={switchToRegisterPage} className='register-click'>here</span></p>
         </div>
     )
